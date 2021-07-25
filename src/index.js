cf88c3d6a822a50a0e8c58d3b36ec9dde0402436
Eunchong Kim <chariskimec@gmail.com>
2021-07-25 18:57:31 +0900

    Added title

--- src/index.js
+++ src/index.js
@@ -23,12 +23,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const bkg = new BasicCanvas(0, 0, global.uno_game_w, global.uno_game_h);
   bkg.canvas.classList.add('uno-game-canv-bkg');
 
+  // Create UNO title
+  const title = document.createElement('h1');
+  title.innerHTML = 'UNO Game'
+  title.classList.add('uno-game-typewriter')
+  title.style.fontSize = global.uno_game_h/6 + 'px';
+  title.style.top = global.uno_game_h/3 + 'px';
+  title.style.zIndex = global.canvas_count;
+  global.canvas_count++;
+  global.uno_game_div.appendChild( title );
+
   // Create room button
   const create_room_box_text = new BoxText(global.uno_game_w*5/12, global.uno_game_h*3/4,
       global.uno_game_w/6, global.uno_game_w/12, 'Create Game');
-
-  /* Canvas click */
   create_room_box_text.canvas.addEventListener("click", e => {
+    // Remove elements
+    title.remove();
     create_room_box_text.remove();
     createRoom();
   });
